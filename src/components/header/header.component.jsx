078450e7b2f62abfb1500f5ac2b5c009134aa1fc
Sam Feighery <sfeighery@gmail.com>
2021-03-01 19:56:51 +0000

    Implemented firebase utils, included ability to store aunthenticated users into the firestore database

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -9,12 +9,11 @@ import './header.styles.scss';
 
 const Header = ({ currentUser }) => (
     <div className='header'>
-        <Link classname='logo-container' to="/">
+        <Link className='logo-container' to="/">
             <Logo className='logo' />
         </Link>
         <div className='options'>
             <Link className='option' to='/shop'>SHOP</Link>
-            <Link className='option' to='/shop'>CONTACT</Link>
             {
                 currentUser ?
                 <div className='option' onClick={() => auth.signOut()}>
